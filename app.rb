cdae3c5f4686bf7489fb53951be7f8b3ac52ba1e
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-02-03 16:55:39 +0000

    added sessions

--- app.rb
+++ app.rb
@@ -2,16 +2,30 @@ require 'sinatra/base'
 require 'shotgun'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
-  post '/play' do
-  	p params
-  	@name_one = params[:name_one]
-  	@name_two = params[:name_two]
+  post '/names' do
+    @name_one = session[:name_one].inspect
+    @name_two = session[:name_two].inspect
+    # @name_one = params[:name_one]
+  	# @name_two = params[:name_two]
+    # erb(:play)
+    redirect to('/play')
+  end
+
+  get '/play' do
+    p params
+    #erb(:play)
+    session['name_one'] = params[:name_one]
+    session['name_two'] = params[:name_two]
     erb(:play)
   end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
